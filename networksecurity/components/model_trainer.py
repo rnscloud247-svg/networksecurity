4dfa77b0ebb3d4dc132142fd3fad4891d47d8286
rnscloud247-svg <rajashekar@Rajashekars-MacBook-Air.local>
2025-08-28 17:05:55 +0530

    all files are changes and working in local

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -66,12 +66,11 @@ class ModelTrainer:
                 # There are other ways to use the Model Registry, which depends on the use case,
                 # please refer to the doc for more information:
                 # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
+                mlflow.sklearn.log_model(best_model, "model", registered_model_name="NetworkSecurityModel")
             else:
                 mlflow.sklearn.log_model(best_model, "model")
 
 
-        
     def train_model(self,X_train,y_train,x_test,y_test):
         models = {
                 "Random Forest": RandomForestClassifier(verbose=1),
